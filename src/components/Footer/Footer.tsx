2e30540a5abbbc3a6c694fda8b5304648546c502
Max Lebedev <dev.runny@gmail.com>
2022-04-24 22:33:14 +0300

    footer section is ready

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,34 @@ import style from "./Footer.module.css"
 
 function Footer() {
   return (
-      <div></div>
+      <footer className={style.background}>
+        <div className="container">
+          <div className={style.footer}>
+            <div className={style.messengerLinks}>
+              <a href="#"><img src="/images/Footer/telegramIcon.svg" alt="telegram" /></a>
+              <a href="#"><img src="/images/Footer/vkIcon.svg" alt="vk" /></a>
+              <a href="#"><img src="/images/Footer/whatsappIcon.svg" alt="whatsapp" /></a>
+              <a href="#"><img src="/images/Footer/viberIcon.svg" alt="viber" /></a>
+            </div>
+            <ul className={style.footerLinks}>
+              <li className={style.footerLink}><a href="#">Область аккредитации</a></li>
+              <li className={style.footerLink}><a href="#">Документы</a></li>
+              <li className={style.footerLink}><a href="#">Запись на ТО</a></li>
+              <li className={style.footerLink}><a href="#">Контакты</a></li>
+            </ul>
+            <div className={style.footerAbout}>
+              <p>Пункт технического осмотра ООО «Делюкс-ПТО»</p>
+              <p>ИНН: 123456854321</p>
+              <p>ОГРН: 123454351468438</p>
+            </div>
+            <div className={style.privacyPolicy}><a href="#">Политика конфиденциальности</a></div>
+            <div className={style.copyright}>
+              <span>© CMS E&L</span>
+              <span>2022</span>
+            </div>
+          </div>
+        </div>
+      </footer>
   );
 }
 
