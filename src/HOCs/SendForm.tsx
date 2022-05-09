38bab91717e0770bec680b5d0b59665d91bb4686
Roman Evgenov <Roman Evgenov>
2022-05-09 19:59:53 +0400

    Refactoring sendForm

--- src/HOCs/SendForm.tsx
+++ src/HOCs/SendForm.tsx
@@ -1,47 +1,23 @@
-import React, {useEffect, useRef, useState} from "react";
-import SendForm from "../components/SendForm/SendForm";
-import {getAccreditation} from "../API/acccreditation";
-import {Category} from "../types/accreditation";
-import {useTypedSelector} from "../hooks/useTypedSelector";
+import { FormItemProps } from "../components/FormItem/FormItem";
 
+type InjectedProps = {};
 
-const withContainerSendForm = (Component: React.ComponentType) => {
-  return () => {
-
-    const currentCategories = useTypedSelector(state => state.accreditation.categories)
-    const categoryRef = useRef<Category[] | null>(null)
-    categoryRef.current = currentCategories
-
-    const [categories, setCategories] = useState<Category[]>([] as Category[])
-    const [isFetching, setIsFetching] = useState<boolean>(false)
-
-
-    useEffect(() => {
-      setIsFetching(true)
-      getAccreditation().then(data => setCategories(data)).then(() => setIsFetching(false))
-      console.log(isFetching)
-    }, [])
-
-    // console.log(isFetching)
-
-    if (!currentCategories.length && isFetching) {
-      return (
-          <div>
-            Загрузка....
-          </div>
-      )
-    }
-
-    if (!currentCategories.length) {
-      return <Component />
-    }
-
-    if (!isFetching) {
-      return (
-          <Component />
-      )
-    }
-  }
-}
-
-export const WithFetchSendForm = withContainerSendForm(SendForm)
+export const withSendFormSelect = (
+  SelectComponent: React.ComponentType<FormItemProps>
+) => {
+  return (props: Omit<FormItemProps, keyof InjectedProps>) => {
+    return (
+      <SelectComponent
+        id={props.id}
+        element={props.element}
+        labelName={props.labelName}
+        labelStyle={props.labelStyle}
+		  errors={props.errors}
+		  mainStyleElement={props.mainStyleElement}
+		  register={props.register}
+		  formItemStyle={props.formItemStyle}
+		  selectOptions={props.selectOptions}
+      />
+    );
+  };
+};
---
+++ src/hooks/useSendForm.ts
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { Category } from "../types/accreditation";
+import { ChangeEvent } from "react";
+import { getAccreditation } from "../API/acccreditation";
+import { RoutesName } from "../routes";
+import { useForm } from "react-hook-form";
+
+export const useSendForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: "onSubmit",
+  });
+  const [categories, setCategories] = useState<Category[]>([] as Category[]);
+  const [category, setCategory] = useState<string>(
+    categories.length ? categories[0].categoryName : ""
+  );
+  const [currentCategoryDesc, setCurrentCategoryDesc] = useState<
+    string | undefined
+  >(categories.length ? categories[0].categoryDescription : "");
+  const navigate = useNavigate();
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  const onChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+    setCurrentCategoryDesc(check(e.target.value));
+  };
+
+  const check = (option: string) => {
+    const currentCategory = categories.find(
+      (cat) => cat.categoryName === option
+    );
+    if (currentCategory && currentCategory.categoryDescription) {
+      return currentCategory.categoryDescription;
+    }
+  };
+
+  const onClickOverlay = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === overlayRef.current) {
+      navigate(RoutesName.MAIN);
+    }
+  };
+
+  useEffect(() => {
+    getAccreditation().then((data) => {
+      const categories = data.map((item) => item);
+      setCategories(categories);
+      setCurrentCategoryDesc(categories[0].categoryDescription);
+    });
+  }, []);
+
+  return {
+    onChangeCategory,
+    currentCategoryDesc,
+    category,
+    categories,
+    onClickOverlay,
+    overlayRef,
+    navigate,
+    register,
+    handleSubmit,
+    errors,
+    reset,
+  };
+};
