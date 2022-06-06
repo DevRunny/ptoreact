2f9f2dba33b2bf136b3e0fe885e7b5994b7fa76b
Max Lebedev <dev.runny@gmail.com>
2022-06-06 16:34:18 +0300

    leftSideBar was fixed

--- src/components/AdminPanel/LeftSideBar/LeftSideBar.tsx
+++ src/components/AdminPanel/LeftSideBar/LeftSideBar.tsx
@@ -14,50 +14,52 @@ function LeftSideBar() {
     navigate("/login")
   }
   return (
-      <div className={style.leftSideBar}>
-        <div className={style.menu}>
-          <div className={style.logoAdmin}>
-            <img src={adminPanelImages.logo.src} alt={adminPanelImages.logo.alt} />
-            <h2 className={style.title}>Admin Panel</h2>
+      <div className={style.leftSideBarWrap}>
+        <div className={style.leftSideBar}>
+          <div className={style.menu}>
+            <div className={style.logoAdmin}>
+              <img src={adminPanelImages.logo.src} alt={adminPanelImages.logo.alt} />
+              <h2 className={style.title}>Admin Panel</h2>
+            </div>
+            <hr />
+            <nav className={style.navBar}>
+              <Link
+                  image={adminPanelImages.info.src}
+                  alt={adminPanelImages.info.alt}
+                  linkText={"Информация"}
+                  url={"/admin"} />
+              <Link
+                  image={adminPanelImages.accredit.src}
+                  alt={adminPanelImages.accredit.alt}
+                  linkText={"Область аккредитации"}
+                  url={"/admin/accreditation"} />
+              <Link
+                  image={adminPanelImages.doc.src}
+                  alt={adminPanelImages.doc.alt}
+                  linkText={"Документы"}
+                  url={"/admin/documents"} />
+              <Link
+                  image={adminPanelImages.address.src}
+                  alt={adminPanelImages.address.alt}
+                  linkText={"Адрес"}
+                  url={"/admin/address"} />
+              <Link
+                  image={adminPanelImages.messengers.src}
+                  alt={adminPanelImages.messengers.alt}
+                  linkText={"Мессенджеры"}
+                  url={"/admin/messengers"} />
+              <Link
+                  image={adminPanelImages.settings.src}
+                  alt={adminPanelImages.settings.alt}
+                  linkText={"Настройки"}
+                  url={"/admin/settings"} />
+            </nav>
+            <hr />
           </div>
-          <hr />
-          <nav className={style.navBar}>
-            <Link
-                image={adminPanelImages.info.src}
-                alt={adminPanelImages.info.alt}
-                linkText={"Информация"}
-                url={"/admin"} />
-            <Link
-                image={adminPanelImages.accredit.src}
-                alt={adminPanelImages.accredit.alt}
-                linkText={"Область аккредитации"}
-                url={"/admin/accreditation"} />
-            <Link
-                image={adminPanelImages.doc.src}
-                alt={adminPanelImages.doc.alt}
-                linkText={"Документы"}
-                url={"/admin/documents"} />
-            <Link
-                image={adminPanelImages.address.src}
-                alt={adminPanelImages.address.alt}
-                linkText={"Адрес"}
-                url={"/admin/address"} />
-            <Link
-                image={adminPanelImages.messengers.src}
-                alt={adminPanelImages.messengers.alt}
-                linkText={"Мессенджеры"}
-                url={"/admin/messengers"} />
-            <Link
-                image={adminPanelImages.settings.src}
-                alt={adminPanelImages.settings.alt}
-                linkText={"Настройки"}
-                url={"/admin/settings"} />
-          </nav>
-          <hr />
-        </div>
-        <div className={style.logOut}>
-          <img src={adminPanelImages.decor.src} alt={adminPanelImages.decor.alt} />
-          <span className={style.logOutButton} onClick={onClickExitUser}><img src={adminPanelImages.logout.src} alt={adminPanelImages.logout.alt} />Выйти</span>
+          <div className={style.logOut}>
+            <img src={adminPanelImages.decor.src} alt={adminPanelImages.decor.alt} />
+            <span className={style.logOutButton} onClick={onClickExitUser}><img src={adminPanelImages.logout.src} alt={adminPanelImages.logout.alt} />Выйти</span>
+          </div>
         </div>
       </div>
   );
