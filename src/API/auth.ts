b11f92f886c6f0350ee67bd172a2dcfffcf97417
Roman Evgenov <Roman Evgenov>
2022-06-08 14:13:21 +0400

    Добавлено изменение логина и пароля при нажатии на кнопку сохранить

--- src/API/auth.ts
+++ src/API/auth.ts
@@ -5,3 +5,12 @@ export const getUser = async (): Promise<User> => {
   const response = await axios.get("https://6239d76d28bcd99f0275d5f1.mockapi.io/user")
   return response.data[0]
 }
+
+export const changeAdminAccount = async(login: string, password: string): Promise<any> => {
+    const response = await axios.put("https://6239d76d28bcd99f0275d5f1.mockapi.io/user/1", {
+      id: 1,
+      login,
+      password
+  })
+  return response.data
+}
--- src/components/AdminPanel/Pages/Settings/Settings.tsx
+++ src/components/AdminPanel/Pages/Settings/Settings.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AdminMainTitle from "../AdminMainTitle";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import style from "./Settings.module.css"
 import AdminFormItem, {InputType} from "../AdminFormItem/AdminFormItem";
+import {useActions} from "../../../../hooks/useActions";
+import {useNavigate} from "react-router-dom";
+import {changeAdminAccount} from "../../../../API/auth";
 
 const Settings = () => {
-  const onClickSave = (id: string, inputValue: string, inputType?: InputType) => {
-    console.log(inputValue)
+
+    const {logout, setUser} = useActions()
+    const navigate = useNavigate()
+
+    const getUserLogin = (): string => {
+        const login = localStorage.getItem("login")
+        if (login) {
+            return login
+        } else {
+            logout()
+            navigate("login")
+            return ""
+        }
+    }
+    const getUserPassword = (): string => {
+        const password = localStorage.getItem("password")
+        if (password) {
+            return password
+        } else {
+            logout()
+            navigate("login")
+            return ""
+        }
+    }
+
+
+  const onClickSave = async (id: string, inputValue: string, inputType?: InputType) => {
+    switch (id) {
+        case "login":
+            const responseLogin = await changeAdminAccount(inputValue, getUserPassword())
+            setUser(responseLogin)
+            localStorage.setItem("login", inputValue)
+            break
+        case "password":
+            const responsePassword = await changeAdminAccount(getUserLogin(), inputValue)
+            setUser(responsePassword)
+            localStorage.setItem("password", inputValue)
+            break
+        default:
+            break
+    }
   }
+
+  useEffect(() => {
+      setUser({id: "1", login: getUserLogin(), password: getUserPassword()})
+  }, [])
+
   return (
       <div className={"adminContentBackground"}>
         <AdminMainTitle titleText={"Настройки"} />
@@ -18,8 +65,8 @@ const Settings = () => {
                 mainStyle={"formItem"}
                 inputStyle={style.inputSettings}
                 inputType={"text"}
-                value={"admin@admin.ru"}
-                id={"1"}
+                value={getUserLogin()}
+                id={"login"}
                 onClickSaveFunc={onClickSave}
                 labelText={"Логин(email):"}
                 required={true}
@@ -28,8 +75,8 @@ const Settings = () => {
                 mainStyle={"formItem"}
                 inputStyle={style.inputSettings}
                 inputType={"password"}
-                value={"admin123"}
-                id={"2"}
+                value={getUserPassword()}
+                id={"password"}
                 onClickSaveFunc={onClickSave}
                 labelText={"Пароль:"}
                 required={true}
