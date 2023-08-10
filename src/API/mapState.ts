3ef9103bc2d601ee53224eb502d0ed35aedf7358
Максим Лебедев <dev.runny@gmail.com>
2023-08-10 16:21:44 +0300

    Fix API mapState

--- src/API/mapState.ts
+++ src/API/mapState.ts
@@ -7,16 +7,11 @@ export const getMapState = async (): Promise<any> => {
 };
 
 export const editMapStateCenter = async (centerX: number, centerY: number): Promise<any> => {
-  const mapStateModel = {
-    centerX: centerX,
-    centerY: centerY
-  }
-  const response = await axios.post("http://localhost:5000/mapState/editMapStateCenter", {...mapStateModel})
+  const response = await axios.post("http://localhost:5000/mapState/editMapStateCenter", {centerX: centerX, centerY: centerY})
   return response.data
 };
 
 export const editMapStateZoom = async (zoom: MapStateZoom): Promise<any> => {
-  console.log(zoom)
   const response = await axios.post("http://localhost:5000/mapState/editMapStateZoom", zoom)
   return response.data
 }
